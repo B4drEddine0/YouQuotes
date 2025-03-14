853200e2f4fb4a786010d552a597e92a1e434249
BadrEddine <badrdine03@gmail.com>
2025-03-14 12:12:58 +0000

    feat : terminer le projet avec une implementation de bonus (authentification)

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,28 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuoteController;
+use App\Http\Controllers\ImageController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('quotes', QuoteController::class);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('logout',[AuthController::class, 'logout']);
+    Route::apiResource('quotes', QuoteController::class);
+});
+
+
+
 Route::get('quotes/getByLength/{length}', [QuoteController::class, 'getByLength']);
 Route::get('quotes/getPopular/{nb}', [QuoteController::class, 'getPopular']);
 Route::get('quotes/getRandom/{nb}', [QuoteController::class, 'getRandom']);
+
+
+Route::post('register',[AuthController::class, 'register']);
+Route::post('login',[AuthController::class, 'login']);
+
